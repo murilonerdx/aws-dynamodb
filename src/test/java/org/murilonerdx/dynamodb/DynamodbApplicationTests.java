7e9b84c563d62845310bdd74b56a659f3dac4996
prinln <mu-silva@outlook.com>
2024-08-08 11:15:20 -0300

    feat: Adicionando projeto e configuração inicial

---
+++ src/test/java/org/murilonerdx/dynamodb/DynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.murilonerdx.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
